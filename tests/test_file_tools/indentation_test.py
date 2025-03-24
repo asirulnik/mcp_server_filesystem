e9910b7368a38aded6730f962f34ea67d1aa6fea
Marcus Jellinghaus <50779140+MarcusJellinghaus@users.noreply.github.com>
2025-03-24 11:23:49 +0100

    Fix edit 3 attempt (#10)
    * tests added
    
    * removed fuzzy match
    
    * simplified indentation, removed fuzzy-logic
    
    * further cleanup of fuzzy matching
    
    * code further simplified
    
    * all unit tests working
    
    * updated docu
    
    * Chagned description
    
    * simplified the code
    
    * simplified
    
    * code further simplified
    
    * further simplification
    
    * formatted
    
    * Parameter rename

---
+++ tests/test_file_tools/indentation_test.py
@@ -0,0 +1,8 @@
+def example_function():
+    # This is indented with 4 spaces
+    if new_condition:
+        # Modified comment
+        print("Changed text")
+        for item in items:
+            # This is indented with 12 spaces
+            process(item)
